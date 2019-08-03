631dc80e6e281a4e5e0d74127e29f3ee6e7d3f77
momsse <momssesystem@gmail.com>
2019-08-03 14:38:26 +0200

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# @boilerz/eslint-config
+
+Airbnb + typescript based eslint config.
+
+### Setup
+
+```bash
+npx install-peerdeps --dev @boilerz/eslint-config
+```
+
+`.eslintrc` example:
+```json
+{
+  "extends": "@boilerz/eslint-config"
+}
+```
---
+++ index.js
@@ -0,0 +1,23 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'airbnb',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
+  ],
+  rules: {
+    'react/jsx-filename-extension': 0
+  },
+  settings: {
+    'react': {
+      'pragma': 'React',
+      'version': 'detect'
+    }
+  }
+}
